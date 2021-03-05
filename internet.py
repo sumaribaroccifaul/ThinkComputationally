53826b44358b33dd49d49daf6559042a77c1f519
sumaribaroccifaul <80106832+sumaribaroccifaul@users.noreply.github.com>
2021-03-05 14:56:50 +0200

    updatedinternet.py

--- internet.py
+++ internet.py
@@ -1,14 +1,18 @@
 #!/usr/bin/env python3
 
+# install packages #
 from datetime import datetime
 
+# user instructions #
 start = datetime.now()
 print("Please look up the value of Pi on the internet.")
 print()
 
+# user input #
 val = input("Enter your value: ")
 end = datetime.now()
 
+# print input value #
 print("value:", val)
 print("duration:", end-start)
 
